56a719acc1ba6b8a8c7acf4ecc5fd76f5a533ca0
Simon Murray <spjmurray@yahoo.co.uk>
2016-12-16 07:48:37 +0000

    Day 16

---
+++ 16b.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+input = '10001110011110000'.chars
+length = 35651584
+
+def checksum(x)
+  x.each_slice(2).map{|x, y| x == y ? '1' : '0'}
+end
+
+# Perform the dragon curve until we have the right length
+until input.length >= length
+  input += ['0'] + input.reverse.map{|x| x == '0' ? '1' : '0'}
+end
+
+# Perform 1 round of checksumming
+csum = checksum(input.take(length))
+
+# Continue until we have an odd length
+until csum.length.odd?
+  csum = checksum(csum)
+end
+
+puts csum.join
