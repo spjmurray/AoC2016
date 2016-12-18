41039ea33cf5af0609eda8b25caf8894040f9987
Simon Murray <spjmurray@yahoo.co.uk>
2016-12-18 23:30:31 +0000

    Day 18

---
+++ 18a.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+input = '......^.^^.....^^^^^^^^^...^.^..^^.^^^..^.^..^.^^^.^^^^..^^.^.^.....^^^^^..^..^^^..^^.^.^..^^..^^^..'.chars
+depth = 40
+
+class Array
+  def calc
+    self.reverse == self ? '.' : '^'
+  end
+
+  def safe
+    self.count{|x| x == '.'}
+  end
+end
+
+count = input.safe
+depth.pred.times do
+  input = (['.'] + input + ['.']).each_cons(3).map(&:calc)
+  count += input.safe
+end
+
+puts count
