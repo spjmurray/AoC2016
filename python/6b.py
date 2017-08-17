b2d0f38f715e32f5b3a1744274135a7bda7bad79
Simon Murray <simon.murray@datacentred.co.uk>
2017-08-17 16:19:10 +0100

    Day 6

---
+++ python/6b.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from collections import Counter
+
+def main():
+    # Read the input and explode the lines discarding the newline
+    data = [list(x)[:-1] for x in open('6.in')]
+    # Transpose
+    data = zip(*data)
+    # Count the characters in each tuple
+    data = [Counter(x) for x in data]
+    # Sort based on frequency
+    data = [sorted(x, key=lambda y: x[y]) for x in data]
+    # Answer is all the least popular characters
+    print ''.join(x[0] for x in data) 
+    
+
+if __name__ == '__main__':
+    main()
+
+# vi: ts=4 et:
