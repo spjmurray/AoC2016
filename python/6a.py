57eeeb336eda4948107b6b37fd8d2b160bfb5a11
Simon Murray <simon.murray@datacentred.co.uk>
2017-08-18 11:14:50 +0100

    Day 6 Improvements

--- python/6a.py
+++ python/6a.py
@@ -2,18 +2,15 @@
 
 from collections import Counter
 
+
 def main():
     # Read the input and explode the lines discarding the newline
-    data = [list(x)[:-1] for x in open('6.in')]
+    data = [list(x.strip()) for x in open('6.in')]
     # Transpose
     data = zip(*data)
-    # Count the characters in each tuple
-    data = [Counter(x) for x in data]
-    # Sort based on frequency
-    data = [sorted(x, key=lambda y: x[y], reverse=True) for x in data]
-    # Answer is all the most popular characters
-    print ''.join(x[0] for x in data) 
-    
+    # Count the characters in each tuple; answer is all the most popular characters
+    print ''.join(Counter(x).most_common()[0][0] for x in data)
+
 
 if __name__ == '__main__':
     main()
