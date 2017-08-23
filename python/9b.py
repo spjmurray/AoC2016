f7ed9cc958e1bd790f93294b17c332a6f8d33770
Simon Murray <simon.murray@datacentred.co.uk>
2017-08-23 09:45:31 +0100

    Day 9

---
+++ python/9b.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+def decompress(data):
+    """
+    Iterate through an iterable type e.g. string or character
+    array looking for RLE control sequences and recursively
+    decompressing and expanding the next N characters M times.
+    Return the length.
+    """
+    length = 0
+    iterator = iter(data)
+    for char in iterator:
+        # Control sequence
+        if char == '(':
+            # Collect the control fields
+            control = ""
+            while True:
+                peek = iterator.next()
+                if peek == ')':
+                    break
+                control += peek
+            take, times = [int(x) for x in control.split('x')]
+            # Extract 'take' characters and repeat 'times' times
+            length += decompress(iterator.next() for x in range(0, take)) * times
+        else:
+            length += 1
+    return length
+
+
+def main():
+    # Load the input
+    data = open('9.in').read().strip()
+    # Answer is the length of the output
+    print decompress(data)
+
+
+if __name__ == '__main__':
+    main()
+
+# vi: ts=4 et:
