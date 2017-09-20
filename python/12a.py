95916ea1095e82b364cb76c2153b225ed2b6b11b
Simon Murray <simon.murray@datacentred.co.uk>
2017-09-20 11:28:36 +0100

    Day 12 Python

---
+++ python/12a.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import collections
+
+def main():
+    # Our VM has an instruction pointer and 4 registers a-d
+    ip = 0
+    registers = collections.defaultdict(int)
+
+    # Load in the assembler and tokenize
+    instructions = [x.split() for x in open('12.in')]
+
+    # Return immediate value or a register reference
+    def load(ref):
+        try:
+            return int(ref)
+        except ValueError:
+            return registers[ref]
+
+    # Execute until we access an invalid instruction
+    try:
+        while True:
+            # Get the instruction at the IP
+            instruction = instructions[ip]
+            # Unpack the opcode and parameters
+            opcode, params = instruction[0], instruction[1:]
+            # By default all instructions increment the IP by one
+            ip_next = 1
+            # Copy from register or immediate to a register
+            if opcode == 'cpy':
+                registers[params[1]] = load(params[0])
+            # Increment a register
+            elif opcode == 'inc':
+                registers[params[0]] += 1
+            # Decrement a register
+            elif opcode == 'dec':
+                registers[params[0]] -= 1
+            # Jump an immediate distance if a register or immediate is non zero
+            elif opcode == 'jnz':
+                if load(params[0]):
+                    ip_next = int(params[1])
+            # Increment the IP
+            ip += ip_next
+    except IndexError:
+        pass
+
+    print registers
+
+if __name__ == '__main__':
+    main()
+
+# vi: ts=4 et:
